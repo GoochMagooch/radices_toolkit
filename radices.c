21c5075904e3a70c7b94a8f818e3438ecc130bfa
Magooch <crvallecito@gmail.com>
2026-04-12 16:57:46 -0600

    Return letter digits, if necessary for single multipliers - Ln 861 & 869

--- radices.c
+++ radices.c
@@ -858,7 +858,7 @@ void calc_mul(int *num1, int *num2, int iterator, int r, int muls) {
             printf("\nProduct: ");
             for (int i = 0; i < muls; i++) {
                 for (int j = 0; j < index; j++) {
-                    printf("%d ", master_product_array[i][j]);
+                    printf("%c ", return_char(master_product_array[i][j]));
                 }
             }
             printf("\n");
@@ -866,7 +866,7 @@ void calc_mul(int *num1, int *num2, int iterator, int r, int muls) {
             printf("\nProduct: ");
             for (int i = 0; i < muls; i++) {
                 for (int j = 0; j < iterator; j++) {
-                   printf("%d ", master_product_array[i][j]);
+                   printf("%c ", return_char(master_product_array[i][j]));
                 }
             }
             printf("\n");
