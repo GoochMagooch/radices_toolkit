583914a90a104ef4cd4bd2038ca2b422c6642442
Magooch <crvallecito@gmail.com>
2026-06-07 21:26:02 -0600

    Next step in debugging - Ln 787

--- radices.c
+++ radices.c
@@ -784,6 +784,7 @@ void calc_mul(int *num1, int *num2, int iterator, int r, int muls) {
                     //      Every temp_product and temp_conversion is incorrect
                     //      Some calculations work, and some don't, so I'm not sure it's this condition. Maybe something in calc_add() with certain numbers?
                     //      Now I need to figure out a way to test this
+                    //      Do the math on paper first, and see what doesn't align
                     printf("CONDITION ONE: \n");
                     printf("temp_product: %d\n", temp_product);
                     printf("temp_conversion: %d\n", temp_conversion);
