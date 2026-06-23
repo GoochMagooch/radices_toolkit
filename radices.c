aba9e35a307761b28a672a0e56104f769d2ecd63
Magooch <crvallecito@gmail.com>
2026-06-23 00:38:01 -0600

    New detail in current bug - Ln 846

--- radices.c
+++ radices.c
@@ -843,6 +843,7 @@ void calc_mul(int *num1, int *num2, int iterator, int r, int muls) {
                         printf("temp_conversion: %d\n", temp_conversion);
                         temp_quotient = (temp_conversion - temp_quotient) / 100; // FIX: ACCURATELY RETURNS RESULT OF ABB * A (16), BUT NOT ABB * F (16)
                                                                                  //      DO THE MATH FOR ABB * F AND ANALYZE OUTPUT OF PROGRAM
+                                                                                 //      FIRST temp_product is 165, 105 (16), SEPARATES INTO 1 AND 5 INSTEAD 10 AND 5
                         printf("temp_quotient: %d\n", temp_quotient);
                         temp_product = (temp_conversion - (temp_quotient * 100)); // FIX: NEEDS MODIFYING
                         printf("temp_product after quotient: %d\n", temp_product);
