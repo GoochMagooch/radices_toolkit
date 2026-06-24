4a6f33b631c31cff077c7d59e90b73ad92c5031c
Magooch <crvallecito@gmail.com>
2026-06-24 15:39:27 -0600

    Ln 848

--- radices.c
+++ radices.c
@@ -841,10 +841,12 @@ void calc_mul(int *num1, int *num2, int iterator, int r, int muls) {
                         printf("CONDITION TWO RADIX > 9: \n");
                         printf("temp_product: %d\n", temp_product);
                         printf("temp_conversion: %d\n", temp_conversion);
-                        temp_quotient = (temp_conversion - temp_quotient) / 100; // FIX: ACCURATELY RETURNS RESULT OF ABB * A (16), BUT NOT ABB * F (16)
-                                                                                 //      DO THE MATH FOR ABB * F AND ANALYZE OUTPUT OF PROGRAM
-                                                                                 //      FIRST temp_product is 165, 105 (16), SEPARATES INTO 1 AND 5 INSTEAD 10 AND 5
-                                                                                 //      106 / 100 == 1 not 10
+                        // FIX: ACCURATELY RETURNS RESULT OF ABB * A (16), BUT NOT ABB * F (16)
+                        //      DO THE MATH FOR ABB * F AND ANALYZE OUTPUT OF PROGRAM
+                        //      FIRST temp_product is 165, 105 (16), SEPARATES INTO 1 AND 5 INSTEAD 10 AND 5
+                        //      106 / 100 == 1 not 10
+                        //      :NOOOOOOOOOO:
+                        temp_quotient = (temp_conversion - temp_quotient) / 10;
                         printf("temp_quotient: %d\n", temp_quotient);
                         temp_product = (temp_conversion - (temp_quotient * 100)); // FIX: NEEDS MODIFYING
                         printf("temp_product after quotient: %d\n", temp_product);
