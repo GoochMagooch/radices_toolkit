5680466beeb2b0ce699c76ab177ca0401342f4f5
Magooch <crvallecito@gmail.com>
2026-07-20 22:56:56 -0600

    Possible solution - Ln 821

--- radices.c
+++ radices.c
@@ -818,6 +818,7 @@ void calc_mul(int *num1, int *num2, int iterator, int r, int muls) {
                             //                ORDER TO TRIGGER EITHER MATHEMATICAL OPERATION
                             //                114 (16) could be 1E or it could be B4
                             //                What signal would determine that the ones place is double digits or a single digit?
+                            //                Perhaps some type of comparison between temp_product and temp_conversion
                             //
                             //      If I have an array, instead of a value, will other parts of this code break?
                             //      I have a line of code below: temp_product_arr[j] = temp_product;
