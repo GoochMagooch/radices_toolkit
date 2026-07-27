f8d81d4f87de38283dcfd6ab124c2caaaeb96935
Magooch <crvallecito@gmail.com>
2026-07-26 23:13:23 -0600

    Next thing to work on - Ln 838

--- radices.c
+++ radices.c
@@ -835,7 +835,7 @@ void calc_mul(int *num1, int *num2, int iterator, int r, int muls) {
                         printf("CONDITION THREE: \n");
                         printf("temp_product: %d\n", temp_product);
                         printf("temp_conversion: %d\n", temp_conversion);
-                        temp_quotient = temp_conversion / 10; // XXX: THIS WILL SEPARATE 1B (16) INTO 11
+                        temp_quotient = temp_conversion / 10; // XXX: THIS WILL SEPARATE 1B (16) INTO 11 - how can I test this?
                         printf("temp_quotient: %d\n", temp_quotient);
                         temp_product = temp_conversion - (temp_quotient * 10); // XXX: THIS WILL SEPARATE 1B (16) INTO 0
                         printf("temp_product after quotient: %d\n", temp_product);
