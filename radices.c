1494b8b8a43acfa6fc8dd9483d0120afeed499c2
Magooch <crvallecito@gmail.com>
2026-09-02 08:29:43 -0600

    More notes - Ln 1076

--- radices.c
+++ radices.c
@@ -1072,7 +1072,8 @@ void calc_div(int *num1, int *num2, int iterator, int r) {
     //          I could...I'm not sure, it all comes back to how the computer calcuates in base 10.
     //          I think I'm going to have to convert.
     //          It couldn't just be as simple as using base 10 to see how many times 21 (3) goes into 210 (3), because that would be 10,
-    //          which...is 3 in base 3
+    //          which...is 3 in base 3. Does this work with other calculations? It won't, because if something goes in 9 times in 
+    //          base 10, there is no 9 in base 3. But I can test anyway I suppose.
 
     // NOTE: if the dividend is 10010 and the first operation is to check if the divisor fits into 1 and it doesn't,
     //       the next step is to check if the divisor fits into 10, but the computer will see that as 10 (base 10)
