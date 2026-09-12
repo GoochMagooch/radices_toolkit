e9cddf9b45c36baa0abdd8881cd1236938422771
Magooch <crvallecito@gmail.com>
2026-09-12 11:22:22 -0600

    Confirmation of assumption - Ln 1063

--- radices.c
+++ radices.c
@@ -1060,6 +1060,7 @@ void calc_div(int *dividend, int *divisor, int iterator, int r) {
         // calculation to find how many times the divisor goes into temp_dividend?
         // it would need to be looped right?
         // how many times would it loop? the amount of digits in the dividend I think...
+        // yes, because the dividend will be run against every individual dividend digit, starting at the first, and continuing with each subsequent digit
     // }
     // XXX: QUESTION: how to check if a divisor fits into a dividend without conversion?
     //      For binary it's simple to see if a divisor fits into a dividend. It either does or it doesn't.
