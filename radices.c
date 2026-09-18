065cbfb0f45050fc61dd2f08e4d1d348e6559d86
Magooch <crvallecito@gmail.com>
2026-09-17 23:50:28 -0600

    Possible solution for current step - Ln 1061

--- radices.c
+++ radices.c
@@ -1058,6 +1058,7 @@ void calc_div(int *dividend, int *divisor, int iterator, int r) {
     //    if (divisor_integer > temp_dividend) { // NOTE: IN RADIX
     //        calculation to combine next element in dividend_digits
     //        FIX: WORK ON THIS NEXT
+    //             set a variable and add each element to that variable?
     //    } else {
     //        calculation to find how many times the divisor goes into temp_dividend?
     //        it would need to be looped right?
