9308207e961ce71055777aec300785826d352a51
Magooch <crvallecito@gmail.com>
2026-09-19 16:19:33 -0600

    Calculation to increment temp_dividend - Ln 1062

--- radices.c
+++ radices.c
@@ -1059,7 +1059,7 @@ void calc_div(int *dividend, int *divisor, int iterator, int r) {
     //        calculation to combine next element in dividend_digits
     //        FIX: WORK ON THIS NEXT
     //             set a variable and add each element to that variable?
-    //             dividend[i] + dividend_digit_holder (?)
+    //             temp_dividend = dividend[i+1] + temp_dividend;
     //    } else {
     //        calculation to find how many times the divisor goes into temp_dividend?
     //        it would need to be looped right?
